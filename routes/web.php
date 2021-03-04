61563545d34ae4b4506e491542ef3fa0af38e0a9
Rubén M <dev.mantecon@gmail.com>
2021-03-04 22:23:25 +0100

    build: Rework some changes after breeze install
    Typical shit that happens when setting up initial environments among peers

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+	return view('styletesting');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+
 
 require __DIR__.'/auth.php';
