7462cf21954e487f5572b9212ff38bf2059c4c45
Rubén M <dev.mantecon@gmail.com>
2021-03-05 16:29:42 +0100

    style:fix: Disable Preflight for no css interference
    Tailwind comes with a default css theme, Preflight.
    It's very, very simple, but it kept interfering with water.css.
    Tried disabling it to avoid specificity issues.
    This should not affect using and styling with Tailwind in any way

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
+    corePlugins: {
+        preflight: false,
+    },
     purge: [
         "./storage/framework/views/*.php",
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
-    darkMode: 'media', // or false or 'class'
+    darkMode: "class", // or false or 'class'
     theme: {
-        extend: {colors:{"leFocus": "var(--focus)"}},
+        extend: {},
     },
     variants: {
         extend: {},
