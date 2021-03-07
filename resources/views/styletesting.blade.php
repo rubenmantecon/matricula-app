e10d67f82336931bc5e9b15e0ac383bef775958c
Rubén M <dev.mantecon@gmail.com>
2021-03-07 14:27:00 +0100

    perf: :art: Add styles and scripts

--- resources/views/styletesting.blade.php
+++ resources/views/styletesting.blade.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/water.css') }}">
+	<script src="{{ asset('js/app.js') }}"></script>
 	<title>Loooreeem Ipsuuuuum </title>
 </head>
 
