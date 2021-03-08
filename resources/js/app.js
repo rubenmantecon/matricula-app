a0055ef1970465ae3754d81b88ddc4d4c0ebbeee
Rubén M <dev.mantecon@gmail.com>
2021-03-08 23:05:29 +0100

    refactor: :art: Rearrange location of scripts for Laravel Mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
-import $ from "jquery";
-window.$ = window.jQuery = $;
-
 require("./bootstrap");
-require("../../public/js/jquery-3.6.0.min");
-require("../../public/js/mindup.editabletable.js");
+require("./scripts.js");
+require("./jquery-3.6.0.min");
+require("./mindup.editabletable.js");
 require("alpinejs");
