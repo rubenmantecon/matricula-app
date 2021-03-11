064a778382e51c7511dab75a7a44ea1e12b532df
Rubén M <dev.mantecon@gmail.com>
2021-03-11 19:31:33 +0100

    refactor:change some js and css from resources/

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,6 +12,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import $ from "jquery";
 window.$ = window.jQuery = $;
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
