a46ed96331fd0f365d13e3ebeee179f70840b818
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2021-03-14 17:37:29 +0100

    css fixes and fiveicon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,16 +26,16 @@
 
                         <!-- Email Address -->
                         <div>
-                            <x-label for="email" :value="__('Email')" />
+                            <label for="email" :value="__('Email')" class="text-white">Usuari</label>
 
-                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                            <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
                         </div>
 
                         <!-- Password -->
                         <div class="mt-4">
-                            <x-label for="password" :value="__('Password')" />
+                            <label for="password" :value="__('Password')" class="text-white">Contraseña</label>
 
-                            <x-input id="password" class="block mt-1 w-full"
+                            <input id="password" class="block mt-1 w-full"
                                             type="password"
                                             name="password"
                                             required autocomplete="current-password" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/water.css') }}">
 	<script src="{{ asset('js/app.js') }}"></script>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('/img/icon.png') }}">
     <title>Matricula-app</title>
 </head>
 <body>
@@ -14,10 +15,11 @@
     <main class="flex items-center text-center">
         <div class="center full">
             <div class="center">
-                <h1>MATRICULA</h1>
+                <h1>IES ESTEVE TERRADAS I ILLA</h1>
             </div>
-            <div>
+            <div class="mt-10">
                 <h3>Benvinguts a la plataforma per fer la matriculació del centre INS Esteve Terradas i Illa.</h3>
+                <img src="{{ asset('/img/icon.png') }}">
             </div>
             <div class="mt-10">
                 <a href="login" accesskey="i" ><button><u>I</u>nicia sessió</button></a>
