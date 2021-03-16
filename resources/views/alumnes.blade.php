a75da8ad56a3577235d1a437e21c42d08cf93453
Alba <alba2000.2014@gmail.com>
2021-03-16 21:28:12 +0100

    Solved problems with import users

--- resources/views/alumnes.blade.php
+++ resources/views/alumnes.blade.php
@@ -3,6 +3,7 @@
 
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="{{ asset('css/water.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -66,41 +67,93 @@
 		</div>
 		<!-- Brief explanation of this whole bunch of buttons -->
 		<!-- Input buttons are really hard to style. So we put the input buttons, hide them, and show some easily stylable buttons. These buttons receive clicks, and when receiving them, through jQuery, simulate a click on the inputs. -->
-		<div class="upload-form">
-			<label for="upload" class="hidden">
-				<input type="file" name="upload" id="fileUpload">
-			</label>
-			<label class="hidden" for="fileSubmit">
-				<input type="submit" value="fileSubmit">
-			</label>
-			<label for="uploadButton">
-				<button name="uploadButton" class="material-icons upload-form__upload-button">file_upload</button>
-			</label>
-		</div>
-	</main>
-	<footer class="flex items-center text-center">
-		<div class="full">
-			<p>© IES Esteve Terradas i Illa</p>
-		</div>
-	</footer>
+        <div class="upload-form">
+            <form>
+                <label for="upload" class="hidden">
+                    <input type="file" name="upload" id="fileUpload">
+                </label>
+                <label class="hidden" for="fileSubmit">
+                    <input type="submit" value="fileSubmit">
+                </label>
+            </form>
+            <label for="uploadButton">
+                <button name="uploadButton" class="material-icons upload-form__upload-button">file_upload</button>
+            </label>
+        </div>
+        <form style="display:none" method="POST" id="hiddenForm" action="{{route('importAlumnes')}}">
+            @csrf
+        </form>
+    </main>
+    <footer class="flex items-center text-center">
+        <div class="full">
+            <p>© IES Esteve Terradas i Illa</p>
+        </div>
+    </footer>
+
 </body>
 <script>
-	/* Upload process' buttons functionality */
-	//Upload file button
-	$(document.body).on('click', '.upload-form__upload-button', function() {
-		$('#fileUpload').click();
-	});
+    /* Upload process' buttons functionality */
+    //Upload file button
+    $(document.body).on('click', '.upload-form__upload-button', function() {
+        $('#fileUpload').click();
+
+    });
+
+    //Detecting when the file is uploaded so we can do the request
+    $(document.body).on('change','#fileUpload',function(){
+        $('#fileSubmit').trigger('click');
+        loadData(importDone);
+
+    })
+  
+    function loadData(actionFunc){
+        var formData= new FormData();
+        formData.append('file',$("input[type=file]")[0].files[0]);
+        console.log(formData);
+
+        //var formData= new FormData(document.getElementById("file"));
+        $.ajax({
+            headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            type:'POST',
+            url:'/admin/dashboard/alumnes',
+            //data: JSON.stringify(data),
+            data:formData,
+            contentType:false,
+            processData:false,
+        }).done(
+            function(data){actionFunc(data)}
+        ).fail(
+            function(jqXHR,textStatus, errorThrown){
+                    
+                    errorFunction(textStatus,errorThrown);
+                }
+        )
+    };
+    function importDone(data){
+        var input;
+        var value;
+        if (data.length>0) {
+            for (var i = 1; i < data.length; i++) {
+                $('#hiddenForm').append('<input type="hidden" name="data'+i+'">');
+                value=JSON.stringify(data[i]);
+                $('input[name="data'+i+'"]').val(value);
+               
+            }
+            $('#hiddenForm').submit();
+        }
+    }
+    function errorFunction (jqXHR,textStatus,errorThrown){
+        alert("Algo ha ido mal : "+ textStatus + errorThrown);
+    };
 
     var page = getUrlParameter('page');
     var totalPages = {{ $alumnes->lastPage() }};
     if (page > totalPages) {
         window.onload = messages('error', 'No se están mostrando estudiantes, vuelva a la pagina correcta');
     }
-    if (page) {
-        window.onload = messages('info', 'Página '+ page + ' mostrant máxim 20 estudiants');
-    } else {
-        window.onload = messages('info', 'Página 1 mostrant máxim 20 estudiants');
-    }
+    window.onload = messages('info', 'Página '+ page + ' mostrant 20 estudiants');
     function messages(code, message) {
         var structure = $("#messages");
         if ($('#messages').children().length == 0) {
@@ -120,45 +173,26 @@
         }
     }
 
-	function messages(code, message) {
-		var structure = $("#messages");
-		if ($('#messages').children().length == 0) {
-			if (code == "success") {
-				structure.addClass("mt-5 mb-5 success");
-				structure.append("<p><b>ÉXIT! | " + message + "</b></p>")
-			} else if (code == "error") {
-				structure.addClass("mt-5 mb-5 errorMSG");
-				structure.append("<p><b>ERROR! | " + message + "</b></p>")
-			} else if (code == "info") {
-				structure.addClass("mt-5 mb-5 info");
-				structure.append("<p><b>INFO! | " + message + "</b></p>")
-			} else if (code == "warning") {
-				structure.addClass("mt-5 mb-5 warning");
-				structure.append("<p><b>ADVERTÈNCIA! | " + message + "</b></p>")
-			}
-		}
-	}
+    function deleteMSG() {
+        $("#messages").children().remove();
+        $("#messages").removeClass();
+    }
 
-	function deleteMSG() {
-		$("#messages").children().remove();
-		$("#messages").removeClass();
-	}
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
 
-	function getUrlParameter(sParam) {
-		var sPageURL = window.location.search.substring(1),
-			sURLVariables = sPageURL.split('&'),
-			sParameterName,
-			i;
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
 
-		for (i = 0; i < sURLVariables.length; i++) {
-			sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) {
+                return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+        return false;
+    };
 
-			if (sParameterName[0] === sParam) {
-				return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-			}
-		}
-		return false;
-	};
 </script>
-
 </html>
