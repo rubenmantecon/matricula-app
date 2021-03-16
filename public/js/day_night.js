efa3232eec3a5231fc1cfd42bc6e498391713579
Rubén M <dev.mantecon@gmail.com>
2021-03-16 22:39:31 +0100

    style | fix: Night mode and its button work ok

--- public/js/day_night.js
+++ public/js/day_night.js
@@ -1,11 +1,28 @@
-const toggleSwitch = document.querySelector('.theme-switcher__switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector(
+	'.theme-switcher__switch input[type="checkbox"]'
+);
 
-	function switchTheme(e) {
-		if (e.target.checked) {
-			document.documentElement.setAttribute('data-theme', 'dark');
-		} else {
-			document.documentElement.setAttribute('data-theme', 'light');
-		}
+$(function(){
+	const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+	const prefersLightScheme = window.matchMedia("(prefers-color-scheme: light)");
+
+	if (prefersDarkScheme.matches) {
+		$(toggleSwitch).click()
 	}
 
-	toggleSwitch.addEventListener('change', switchTheme, false);
+	if (prefersLightScheme.matches) {
+		$(toggleSwitch).click()
+		
+	}
+})
+
+function switchTheme(e) {
+
+	if (e.target.checked) {
+		document.documentElement.setAttribute("data-theme", "dark");
+	} else {
+		document.documentElement.setAttribute("data-theme", "light");
+	}
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
